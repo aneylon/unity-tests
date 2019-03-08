0e64c3d6c7568e2d09aab6b1d4169a7f2bc6392d
aneylon <arlen.m.neylon@gmail.com>
2019-03-07 20:30:26 -0700

    Set up Materials and shader.

---
+++ Assets/Scripts/DisplacementControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplacementControl : MonoBehaviour
+{
+    public float displacementAmount;
+    public ParticleSystem explosionParticles;
+    MeshRenderer meshRenderer;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();    
+    }
+
+    void Update()
+    {
+        displacementAmount = Mathf.Lerp(displacementAmount, 0, Time.deltaTime);
+        meshRenderer.material.SetFloat("_Amount", displacementAmount);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            displacementAmount += 1f;
+            explosionParticles.Play();
+        }
+    }
+}
