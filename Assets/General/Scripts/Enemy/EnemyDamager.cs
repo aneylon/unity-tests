fad01ffc038e7f9d512f70c003fa2957c58a3e0b
aneylon <arlen.m.neylon@gmail.com>
2019-03-16 07:54:26 -0600

    Add basic player and enemy interaction example.

---
+++ Assets/General/Scripts/Enemy/EnemyDamager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class EnemyDamager : MonoBehaviour
+{
+    public int damagePower = 2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit : " + other.gameObject.tag);
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("hit player");
+            PlayerHealth playerHealth = other.gameObject.GetComponent<PlayerHealth>();
+            playerHealth.TakeDamage(damagePower);
+        }
+    }
+}
---
+++ Assets/General/Scripts/Players/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health = 20;
+
+    public void TakeDamage(int damageAmmount)
+    {
+        Debug.Log("Take that!  " + damageAmmount );
+        health -= damageAmmount;
+    }
+
+    private void Die()
+    {
+        Debug.Log("Ded!");
+    }
+
+    private void Update()
+    {
+        if(health <= 0)
+        {
+            Debug.Log("Time to die!");
+            Die();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit : " + other.gameObject.tag);
+    }
+}
